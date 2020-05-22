f8667f0a492045d8c88739b1087b4c4aa706b978
jeetmpatel1 <jeet.mpatel1@gmail.com>
2020-05-21 19:44:05 -0700

    basic graphql query working

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql');
+const {buildSchema} = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/',(req,res,next) => {
+app.use('/graphql',graphqlHttp({
+    schema : buildSchema(`
 
-    res.send('Hello World!');
+            type RootQuery {
+                events: [String!]!
+            }
 
-})
+            type RootMutation {
+                createEvent(name: String): String
+            }
+            schema {
+                query : RootQuery
+                mutation :RootMutation
+            }
+    
+    `),
+    rootValue : {
+        events: () => {
+            return ['Romantic Cokking','Sailing','All Night Coding'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql:true
+    })
+    
+);
+
 app.listen(3000);
--- readme.md
+++ readme.md
@@ -1,4 +1,22 @@
-npm init
-npm install --save express body-parser
-npm install --save-dev nodemon
-npm start
+## Event Manger 
+**Technologies used :** GraphQL, MongoDB, NodeJS , expressJS
+
+### Commands
+> npm init
+> npm install --save express body-parser
+> npm install --save-dev nodemon
+> npm start
+
+> npm install --save express-graphql graphql
+
+Open ```localhost:3000/graphql```
+Put the below query inside  graphql  query editor
+```
+    query {
+      events
+    }
+
+    mutation{
+      createEvent(name:"Sports")
+    }
+```
